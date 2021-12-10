5ee5e70bbcc63b0ad5883a11f1fd7c343c6e6567
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2021-12-10 19:17:57 +1100

    Added basic screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { DashboardScreen } from "./screens/DashboardScreen/DashboardScreen";
+import { LoginScreen } from "./screens/LoginScreen/LoginScreen";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<DashboardScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
